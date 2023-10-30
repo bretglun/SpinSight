aced2be3093820260223671e05d0a05306fd26ae
Johan Berglund <johan.berglund@uu.se>
2023-10-30 15:39:56 +0100

    deploy: add command line option -p for port number

--- deploy.py
+++ deploy.py
@@ -2,10 +2,20 @@
 
 import socket
 import subprocess
+import optparse
 
+
+# parse command line
+p = optparse.OptionParser()
+p.add_option('--port', '-p', default='80',  type="string",
+                help="Port to deploy SpinSight")
+options, arguments = p.parse_args()
+
+# get IP number
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(('8.8.8.8', 80))
 IP = s.getsockname()[0]
 
-print('Deploying app at IP address: ' + IP)
-subprocess.run('panel serve app.py --port 80 --allow-websocket-origin={}:80'.format(IP).split())
+# serve application
+print('Deploying SpinSight at IP address: {} (port {})'.format(IP, options.port))
+subprocess.run('panel serve spinsight/spinsight.py --port {} --allow-websocket-origin={}:{}'.format(options.port, IP, options.port).split())
