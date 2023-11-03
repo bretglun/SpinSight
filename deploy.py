28608c77babdd55ceaffbd46b18c0134f3ce8baa
Johan Berglund <johan.berglund@uu.se>
2023-11-03 10:10:51 +0100

    deploy: add url option

--- deploy.py
+++ deploy.py
@@ -9,6 +9,8 @@ import optparse
 p = optparse.OptionParser()
 p.add_option('--port', '-p', default='80',  type="string",
                 help="Port to deploy SpinSight")
+p.add_option('--url', '-u', default='',  type="string",
+                help="URL identifying server")
 options, arguments = p.parse_args()
 
 # get IP number
@@ -16,6 +18,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(('8.8.8.8', 80))
 IP = s.getsockname()[0]
 
-# serve application
+command = 'panel serve spinsight/spinsight.py --port {} --allow-websocket-origin={}:{}'.format(options.port, IP, 
+options.port)
 print('Deploying SpinSight at IP address: {} (port {})'.format(IP, options.port))
-subprocess.run('panel serve spinsight/spinsight.py --port {} --allow-websocket-origin={}:{}'.format(options.port, IP, options.port).split())
+
+if options.url:
+    command += ' --allow-websocket-origin={}:{}'.format(options.url, options.port)
+    print('Deploying SpinSight at URL: http://{}:{}'.format(options.url, options.port))
+
+# serve application
+subprocess.run(command.split())
