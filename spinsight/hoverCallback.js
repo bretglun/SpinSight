1c15dd252ce51c379d4d7c99f0286cc61e996d72
Johan Berglund <berglund.hpj@gmail.com>
2026-05-31 13:07:13 +0200

    Bugfix hover_index

--- spinsight/hoverCallback.js
+++ spinsight/hoverCallback.js
@@ -1,6 +1,6 @@
 if (cb_data.index.indices.length > 0) {
-    hoverIndex.data = {"index": [cb_data.index.indices[0]], "board": [board]};
+    hover_index.data = {"index": [cb_data.index.indices[0]], "board": [board]};
 }
 else {
-    hoverIndex.data = {"index": [], "board": []};
+    hover_index.data = {"index": [], "board": []};
 }
