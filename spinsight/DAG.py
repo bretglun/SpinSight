ff4d8446f4fd667ef735003b81a65a067cc34535
Johan Berglund <berglund.hpj@gmail.com>
2026-06-29 11:22:20 +0200

    Sort DAG.py

--- spinsight/DAG.py
+++ spinsight/DAG.py
@@ -4,79 +4,6 @@ from functools import partial
 import inspect
 
 
-def equal(a, b):
-    if a is b:
-        return True
-    
-    if type(a) is not type(b):
-        return False
-
-    if isinstance(a, np.ndarray):
-        return np.array_equal(a, b)
-
-    if isinstance(a, dict):
-        if a.keys() != b.keys():
-            return False
-        return all(equal(a[k], b[k]) for k in a)
-
-    if isinstance(a, (list, tuple)):
-        if len(a) != len(b):
-            return False
-        return all(equal(x, y) for x, y in zip(a, b))
-
-    return a == b
-
-
-def topological_order(specs):
-    ts = TopologicalSorter()
-    for name, spec in specs.items():
-        ts.add(name, *spec.get('parents', []))
-    return ts.static_order()
-
-
-class Node:
-    
-    def __init__(self, name, func=None):
-        self.name = name
-        self.func = func
-        
-        self.parents = []
-        self.children = []
-
-        self._valid = False # parent nodes may have changed
-        self._cache = None
-        self.version = 0
-        self.parent_versions = ()
-    
-    def attach(self, parent):
-        self.parents.append(parent)
-        parent.children.append(self)
-    
-    def invalidate(self):
-        if not self._valid:
-            return
-        self._valid = False
-        for child in self.children:
-            child.invalidate()
-
-    @property
-    def value(self):
-        if not self._valid:
-            inputs = [parent.value for parent in self.parents]
-            current_versions = tuple(p.version for p in self.parents)
-            if not self.parents or current_versions != self.parent_versions:
-                self.parent_versions = current_versions
-                self.recompute(inputs)
-            self._valid = True
-        return self._cache
-    
-    def recompute(self, inputs):
-        new = self.func(*inputs)
-        if not equal(self._cache, new):
-            self.version += 1
-            self._cache = new
-
-
 class Graph:
 
     node_specs = {}
@@ -149,6 +76,48 @@ class Graph:
             for node in self.action_nodes[precedence]:
                 node.value
 
+class Node:
+    
+    def __init__(self, name, func=None):
+        self.name = name
+        self.func = func
+        
+        self.parents = []
+        self.children = []
+
+        self._valid = False # parent nodes may have changed
+        self._cache = None
+        self.version = 0
+        self.parent_versions = ()
+    
+    def attach(self, parent):
+        self.parents.append(parent)
+        parent.children.append(self)
+    
+    def invalidate(self):
+        if not self._valid:
+            return
+        self._valid = False
+        for child in self.children:
+            child.invalidate()
+
+    @property
+    def value(self):
+        if not self._valid:
+            inputs = [parent.value for parent in self.parents]
+            current_versions = tuple(p.version for p in self.parents)
+            if not self.parents or current_versions != self.parent_versions:
+                self.parent_versions = current_versions
+                self.recompute(inputs)
+            self._valid = True
+        return self._cache
+    
+    def recompute(self, inputs):
+        new = self.func(*inputs)
+        if not equal(self._cache, new):
+            self.version += 1
+            self._cache = new
+
 
 def print_dependency_chains(source, sink, chain=''):
     if sink.name == source.name:
@@ -159,3 +128,33 @@ def print_dependency_chains(source, sink, chain=''):
     chain = f'-> {sink.name} {chain}'
     for parent in sink.parents:
         print_dependency_chains(source, parent, chain)
+
+
+def topological_order(specs):
+    ts = TopologicalSorter()
+    for name, spec in specs.items():
+        ts.add(name, *spec.get('parents', []))
+    return ts.static_order()
+
+
+def equal(a, b):
+    if a is b:
+        return True
+    
+    if type(a) is not type(b):
+        return False
+
+    if isinstance(a, np.ndarray):
+        return np.array_equal(a, b)
+
+    if isinstance(a, dict):
+        if a.keys() != b.keys():
+            return False
+        return all(equal(a[k], b[k]) for k in a)
+
+    if isinstance(a, (list, tuple)):
+        if len(a) != len(b):
+            return False
+        return all(equal(x, y) for x, y in zip(a, b))
+
+    return a == b
